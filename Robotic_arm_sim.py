582d2ae560e967cd26e4cac45d3cce785599ea57
Grand-Admiral-Thrawn-1 <122584871+Grand-Admiral-Thrawn-1@users.noreply.github.com>
2024-04-22 17:39:19 +0530

    Simulation updated

---
+++ Robotic_arm_sim.py
@@ -0,0 +1,40 @@
+import sys
+from coppeliasim_zmqremoteapi_client import RemoteAPIClient
+import time
+from math import*
+import numpy as np
+import keyboard
+
+if __name__ == '__main__':
+    try:
+        client = RemoteAPIClient()
+        sim = client.require('sim')
+        sim.setStepping(True)
+        sim.startSimulation()
+        print("Started simulation")
+
+        body = sim.getObject("/Robotic_Arm")
+        Servo_1_joint = sim.getObject(
+            "Robotic_Arm\Assembly_URDF\meshes\Servo1")
+        Servo_2_joint = sim.getObject(
+            "Robotic_Arm\Assembly_URDF\meshes\Servo_2")
+        Servo_3_joint = sim.getObject(
+            "Robotic_Arm\Assembly_URDF\meshes\Servo_3")
+        Servo_4_joint = sim.getObject(
+            "Robotic_Arm\Assembly_URDF\meshes\Servo_4")
+
+
+
+        joints = [Servo_1_joint, Servo_2_joint, Servo_3_joint, Servo_4_joint]
+
+
+    except KeyboardInterrupt:
+        sim.stopSimulation()
+        time.sleep(0.5)
+        if sim.getSimulationState() == sim.simulation_stopped:
+            print('\nSimulation interrupted by user in CoppeliaSim.')
+        else:
+            print('\nSimulation could not be interrupted. Stop the simulation manually.')
+
+    finally:
+        sys.exit()
